eb6d66905cfc7452b4aac9d921799287fb967eb8
Hussein Zaher <husseinzaher@outlook.com>
2025-03-22 12:07:03 +0200

    add readme

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ApprovalController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 
 Route::post('/orders', [OrderController::class, 'store']);
 Route::get('/orders/{order}', [OrderController::class, 'show']);
